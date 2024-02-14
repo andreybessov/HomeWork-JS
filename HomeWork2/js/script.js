038387daf1dd5a69f58857d56b453eb8cb1a927d
andreybessov <148684816+andreybessov@users.noreply.github.com>
2024-02-14 18:45:55 +0200

    Add files via upload

---
+++ HomeWork2/js/script.js
@@ -0,0 +1,94 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+// const test = 1;
+// const test = 0;
+// const test = -3;
+// if (test > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+// const test = 'test';
+// const test = "qwerty";
+// const test = true;
+
+// if (test == "test") {
+//     console.log(true);
+// } else {
+//     console.log(false)
+// }
+
+
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+
+// const number = 1;
+// const number = 10;
+// const number = 13;
+// if (number > 10) {
+//     console.log(number - 5);
+// } else {
+//     console.log(number + 5)
+// }
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// const num = Number(prompt('Введіть цифру від 1 до 12'));
+// switch (num) {
+//     case 1:
+//         alert('Січень');  
+//         break
+//     case 2:
+//         alert('Лютий');  
+//         break
+//     case 3:
+//         alert('Березень');
+//         break
+//     case 4:
+//         alert('Квітень');
+//         break
+//     case 5:
+//         alert('Травень');
+//         break
+//     case 6:
+//         alert('Червень'); 
+//         break
+//     case 7:
+//         alert('Липень'); 
+//         break
+//     case 8:
+//         alert('Серпень');  
+//         break
+//     case 9:
+//         alert('Вересень');  
+//         break
+//     case 10:
+//         alert('Жовтень'); 
+//         break
+//     case 11:
+//         alert('Листопад');
+//         break
+//     case 12:
+//         alert('Грудень'); 
+//         break
+// }
+
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+// const num = prompt('Введіть трьохзначне число');
+// const num1 = Number(num[0]);
+// const num2 = Number(num[1]);
+// const num3 = Number(num[2]);
+// if (num > 100 && num < 1000) {
+//     console.log(num1 + num2 + num3);
+// } else {
+//     console.log('Введіть трьохзначне число');
+// }
