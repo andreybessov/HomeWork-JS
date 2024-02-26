11b467c6a64c215114db4bce30cabd3c7d3b1e2a
andreybessov <148684816+andreybessov@users.noreply.github.com>
2024-02-26 20:05:09 +0200

    Add files via upload

---
+++ HomeWork5/js/script.js
@@ -0,0 +1,46 @@
+// Створити телефонну книгу
+// створити початковий клас Abonent, де зберігатимуться ім*я і номер
+// створити set який прийматиме телефон і номер
+// створити get який виводитиме данні про абонента
+// створити три різних юзери
+// вивести данні
+
+// class Abonent {
+//     constructor(data) {
+//         this.name = data.name,
+//         this.number = data.number
+//         this.mobileNumber = data.mobileNumber
+//     }
+//     name = "";
+//     number = "";
+//     mobileNumber = "";
+
+//     set number(newNumber) {
+//         this.number = newNumber,
+//         this.mobileNumber = newNumber
+//     }
+
+//     get userNumber() {
+//     return `Ім'я: ${this.name}, Номер телефону: ${this.mobileNumber}, Номер в телефонній книзі: ${this.number}`
+// }
+// }
+
+// const Igor = new Abonent ({
+//     name: "Igor",
+//     mobileNumber: +380662222222,
+//     number: 5
+// })
+
+// const Maria = new Abonent ({
+//     name: "Maria",
+//     mobileNumber: +380661111111,
+//     number: 2
+// })
+
+// const Andrey = new Abonent ({
+//     name: "Andrey",
+//     mobileNumber: +380660000000,
+//     number: 8
+// })
+
+// console.log(Igor)
