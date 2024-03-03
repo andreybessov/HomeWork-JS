8377c032913fc5236f5cb740f14b74f90c94e786
andreybessov <148684816+andreybessov@users.noreply.github.com>
2024-03-03 10:00:13 +0200

    Add files via upload

---
+++ HomeWork6/js/script.js
@@ -0,0 +1,25 @@
+// const newNumber = new Calculator;
+
+// function Calculator() {
+//     this.num1 = 0;
+//     this.num2 = 0;
+
+//     this.read = function () {
+//         this.num1 = +prompt('Введіть перше число');
+//         this.num2 = +prompt('Введіть друге число');
+//     };
+
+//     this.sum = function () {
+//         return this.num1 + this.num2;
+//     };
+
+//     this.mul = function () {
+//         return this.num1 * this.num2;
+//     };
+// }
+
+// let calculator = new Calculator();
+// calculator.read();
+// console.log('Сума чисел:', calculator.sum());
+// console.log('Добуток чисел:', calculator.mul());
+
