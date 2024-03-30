bdbbfc2428d9c9f8d989b4024ee8950d5b095615
andreybessov <148684816+andreybessov@users.noreply.github.com>
2024-03-30 16:39:01 +0200

    Add files via upload

---
+++ HomeWork7/js/script.js
@@ -0,0 +1,29 @@
+// Завдання 1
+// const button = document.querySelector('#button')
+// const text = document.querySelector('#text')
+
+// const deleteText = () => text.classList.toggle('close')
+
+// button.addEventListener("click", deleteText)
+
+
+
+//Завдання 2
+// const button = document.querySelector('#button')
+
+// const deleteButton = () => button.classList.toggle('close')
+
+// button.addEventListener("click", deleteButton)
+
+//Задвання 3 
+// const parent = document.querySelector('#parent');
+// const child = document.querySelector('#child');
+// const item = document.querySelector('#item');
+
+// const handleClick = (event) => {
+//     console.log(event.target)
+// }
+
+// parent.addEventListener('click', handleClick)
+// child.addEventListener('click', handleClick)
+// item.addEventListener('click', handleClick)
